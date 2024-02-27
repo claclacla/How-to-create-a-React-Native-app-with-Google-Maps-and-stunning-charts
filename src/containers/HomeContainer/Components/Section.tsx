916438b7ba99944e88d916705c544e736f8b00e3
Admin <admin@MBP-di-Admin.fritz.box>
2024-02-27 11:23:35 +0100

    Removed the support for the dark/light mode

--- src/containers/HomeContainer/Components/Section.tsx
+++ src/containers/HomeContainer/Components/Section.tsx
@@ -2,38 +2,26 @@ import type { PropsWithChildren } from 'react';
 
 import {
     Text,
-    useColorScheme,
     View,
     StyleSheet,
 } from 'react-native';
-
-import {
-    Colors,
-} from 'react-native/Libraries/NewAppScreen';
 
 type SectionProps = PropsWithChildren<{
     title: string;
 }>;
 
 function Section({ children, title }: SectionProps): React.JSX.Element {
-    const isDarkMode = useColorScheme() === 'dark';
     return (
         <View style={styles.sectionContainer}>
             <Text
                 style={[
                     styles.sectionTitle,
-                    {
-                        color: isDarkMode ? Colors.white : Colors.black,
-                    },
                 ]}>
                 {title}
             </Text>
             <Text
                 style={[
                     styles.sectionDescription,
-                    {
-                        color: isDarkMode ? Colors.light : Colors.dark,
-                    },
                 ]}>
                 {children}
             </Text>
--- src/containers/HomeContainer/index.tsx
+++ src/containers/HomeContainer/index.tsx
@@ -3,14 +3,12 @@ import React from 'react';
 import {
     ScrollView,
     Text,
-    useColorScheme,
     View,
     StyleSheet,
     Button,
 } from 'react-native';
 
 import {
-    Colors,
     DebugInstructions,
     ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
@@ -23,24 +21,15 @@ import Section from './Components/Section';
 type HomeScreenProps = NativeStackScreenProps<ContainersList, "Home">;
 
 const HomeContainer: React.FC<HomeScreenProps> = (props) => {
-    const isDarkMode = useColorScheme() === 'dark';
-
-    const backgroundStyle = {
-        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    };
-
     return (
         <ScrollView
             contentInsetAdjustmentBehavior="automatic"
-            style={backgroundStyle}>
+            >
             <Button
                 title="Go to Map"
                 onPress={() => props.navigation.navigate('Map')}
             />
-            <View
-                style={{
-                    backgroundColor: isDarkMode ? Colors.black : Colors.white,
-                }}>
+            <View>
                 <Section title="Step One">
                     Edit whenever you do want <Text style={styles.highlight}>App.tsx</Text> to change this
                     screen and then come back to see your edits.
