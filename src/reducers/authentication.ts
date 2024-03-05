a6a7f66790c255548e2c253b0b5fc1888af407d6
Admin <admin@MBP-di-Admin.fritz.box>
2024-03-05 12:10:24 +0100

    Removed the old version of authentication reducer

--- src/reducers/authentication.ts
+++ src/reducers/authentication.ts
@@ -1,32 +1,3 @@
-/*
-import { configureStore } from '@reduxjs/toolkit';
-
-interface IAuthentication {
-    key: string;
-}
-
-const authentication: IAuthentication = {
-    key: "",
-};
-
-const reducer = (state: IAuthentication = authentication, action: any) => {
-    switch (action.type) {
-        case 'SET_KEY':
-            return { ...state, key: state.key };
-        case 'UNSET_KEY':
-            return { ...state, key: "" };
-        default:
-            return state;
-    }
-};
-
-const store = configureStore({
-    reducer: reducer,
-});
-
-export default store;
-*/
-
 import { createSlice } from '@reduxjs/toolkit';
 
 export interface IAuthentication {
